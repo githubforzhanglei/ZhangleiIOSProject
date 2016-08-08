53e2056f1807c3d5a1ac17f54f48ac2b16975adf
githubforzhanglei <zhanglei19910607@163.com>
2016-08-08 17:46:01 +0800

    add cocoaPods

---
+++ ZhangleiIOSProject/ZhangleiIOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhangleiIOSProject
+//
+//  Created by Allone on 16/8/8.
+//  Copyright © 2016年 zhanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZhangleiIOSProject/ZhangleiIOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhangleiIOSProject
+//
+//  Created by Allone on 16/8/8.
+//  Copyright © 2016年 zhanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
